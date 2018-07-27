14444ca94b8d7564cc61093e5964ee279c7fa2a4
alex shehdula <alex@alexs-MacBook-Air.local>
2018-07-27 11:26:25 -0700

    sec

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,23 +1,41 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-	actions: {
-		toggleActive(monster)
-		{
-				monster.toggleProperty('active');
-				monster.save();
-		},
-		createm(monster){
+actions: {
+toggleActive(monster)
+{
+		monster.toggleProperty('active');
+		monster.save();
+},
+createm(){
 
-			let newm = this.get('newtest')
-			let news = this.get('newsect')
-				this.store.createRecord('monster', {
- 	 		name: newm,
-  			level: news
+	let newm = this.get('newtest');
+	let news = this.get('newsect');
 
-		});
-				monster.save();
-		
+	let monster =	this.store.createRecord('monster', {
+ 		name: newm,
+		level: news
+
+
+});
+		console.log(monster);
+		monster.save();
+		console.log(newm);
+		console.log(news);
+		console.log("test");
+		location.reload();
+
+},
+
+	del(monster){
+		 
+		monster.destroyRecord();
+		monster.save();
+		location.reload();
+
+
 	}
+
+
 }	
 });
