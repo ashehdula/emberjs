48f2caef85ba2ec4ec7ff8a99d347b673c41efbb
System Administrator <root@alexs-MacBook-Air.local>
2018-07-30 08:59:13 -0700

    showpage

--- app/router.js
+++ app/router.js
@@ -1,13 +1,16 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-	const Router = EmberRouter.extend({		
-		location: config.locationType,
-		rootURL: config.rootURL
-	});
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
 
-	Router.map(function() {
-		
-	});
+Router.map(function() {
+
+	this.route('monster');
+  this.route('monster', { path: '/monster/:monster_id' });
+  
+});
 
 export default Router;
--- app/routes/monster.js
+++ app/routes/monster.js
@@ -3,17 +3,14 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 		
 
-  model(params) {
-    return this.get('store').findRecord('monster', params.monster_id);
-  },
-
 	model(){
 		return this.store.findAll('monster');
 
 
 
+
+
+
 }
-
-
 
 });
